9eb1df24d55f478bd417a0bf8cb022a2b7798faa
IzzeldeenAsh <ezraashour00@gmail.com>
2022-10-28 15:01:28 +0300

    A17

---
+++ js/Assignement17.js
@@ -0,0 +1,15 @@
+let lorem = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores odit dicta nihil vitae quasi, iure nesciunt consequatur optio temporibus asperiores ipsam iste commodi, fuga earum neque deserunt, quis perferendis molestias.`
+console.log(lorem.endsWith("as."));;
+console.log(lorem.startsWith('Lorem'));
+console.log(lorem.startsWith('ipsum',6));
+console.log(lorem.includes('dolor'));
+console.log(lorem.length);
+console.log(lorem[1]);
+console.log(lorem.charAt(2));
+console.log(lorem.indexOf('m'));
+console.log(lorem.lastIndexOf('m'));
+console.log(lorem.slice(0,5));
+console.log(lorem.slice(4));
+console.log(lorem.split('ipsum'));
+console.log(lorem.substring(1, 4));
+console.log(lorem.slice(3,4));
